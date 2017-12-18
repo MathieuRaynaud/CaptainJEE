19f53c639d88237ec72dc8627524c2d6c7605fa3
Laurent Chasserat <laurent.chasserat@gmail.com>
2017-12-18 10:59:59 +0100

    Fix du servlet

--- src/MyServlet.java
+++ src/MyServlet.java
@@ -9,8 +9,31 @@ public class MyServlet extends HttpServlet{
 
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
 
-        String message = "Transmission de variables : OK !";
-        request.setAttribute( "index", message );
-        this.getServletContext().getRequestDispatcher( "/web/index.jsp" ).forward( request, response );
+
+        response.setContentType("text/html");
+
+        response.setCharacterEncoding( "UTF-8" );
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<!DOCTYPE html>");
+
+        out.println("<html>");
+
+        out.println("<head>");
+
+        out.println("<meta charset=\"utf-8\" />");
+
+        out.println("<title>Test</title>");
+
+        out.println("</head>");
+
+        out.println("<body>");
+
+        out.println("<p>Ceci est une page générée depuis une servlet.</p>");
+
+        out.println("</body>");
+
+        out.println("</html>");
     }
 }
