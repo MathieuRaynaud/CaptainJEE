0c39d4562a1a89db0d1ca533451f724848ff5bfa
MathieuRaynaud <mraynaud@insa-08134>
2017-12-18 10:56:23 +0100

    doGet ajouté à la Servlet

--- src/MyServlet.java
+++ src/MyServlet.java
@@ -1,6 +1,12 @@
 
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
 
 public class MyServlet extends HttpServlet{
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+
+    }
 }
